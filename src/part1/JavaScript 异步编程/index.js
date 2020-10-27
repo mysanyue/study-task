99b8d72d78331ad0e1d5d99cebeae71b7712b912
张伟 <myzony@qq.com>
2020-10-27 22:35:07 +0800

    2020-10-27
    异步编程-完成

--- src/part1/JavaScript 异步编程/index.js
+++ src/part1/JavaScript 异步编程/index.js
@@ -15,9 +15,26 @@
     });
   }
 
-  ajax('/api/user.json').then(function onfulfilled(value) {
-    console.log('onFulfilled', value);
-  }, function onrejected(error) {
-    console.log('onRejected', error);
+  // ajax('/api/user.json').then(function onfulfilled(value) {
+  //   console.log('onFulfilled', value);
+  // }, function onrejected(error) {
+  //   console.log('onRejected', error);
+  // });
+}
+{
+  console.log('global start');
+
+  setTimeout(() => {
+    console.log('setTimeout')
+  }, 0);
+
+  Promise.resolve().then(() => {
+    console.log('promise1');
+  }).then(() => {
+    console.log('promise2');
+  }).then(() => {
+    console.log('promise3');
   });
+
+  console.log('global end');
 }
