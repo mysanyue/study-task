2a131e73d2da80f51552480554de7116e3211fbf
张伟 <myzony@qq.com>
2021-01-22 20:55:00 +0800

    commit

--- part3/task-1/code/snabbdom-list/index.js
+++ part3/task-1/code/snabbdom-list/index.js
@@ -1,119 +1,121 @@
-import { init } from '../../build/package/init.js'
-import { classModule } from '../../build/package/modules/class.js'
-import { propsModule } from '../../build/package/modules/props.js'
-import { styleModule } from '../../build/package/modules/style.js'
-import { eventListenersModule } from '../../build/package/modules/eventlisteners.js'
-import { h } from '../../build/package/h.js'
+import { init } from './build/package/init.js'
+import { classModule } from './build/package/modules/class.js'
+import { propsModule } from './build/package/modules/props.js'
+import { styleModule } from './build/package/modules/style.js'
+import { eventListenersModule } from './build/package/modules/eventlisteners.js'
+import { h } from './build/package/h.js'
+import dataApi from './data.js'
 
-var patch = init([classModule, propsModule, styleModule, eventListenersModule])
+const patch = init([classModule, propsModule, styleModule, eventListenersModule])
 
-var vnode
+let data = dataApi;
+let vnode = null;
 
-var nextKey = 11
-var margin = 8
-var sortBy = 'rank'
-var totalHeight = 0
-var originalData = [
-  { rank: 1, title: 'The Shawshank Redemption', desc: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', elmHeight: 0 },
-  { rank: 2, title: 'The Godfather', desc: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', elmHeight: 0 },
-  { rank: 3, title: 'The Godfather: Part II', desc: 'The early life and career of Vito Corleone in 1920s New York is portrayed while his son, Michael, expands and tightens his grip on his crime syndicate stretching from Lake Tahoe, Nevada to pre-revolution 1958 Cuba.', elmHeight: 0 },
-  { rank: 4, title: 'The Dark Knight', desc: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.', elmHeight: 0 },
-  { rank: 5, title: 'Pulp Fiction', desc: 'The lives of two mob hit men, a boxer, a gangster\'s wife, and a pair of diner bandits intertwine in four tales of violence and redemption.', elmHeight: 0 },
-  { rank: 6, title: 'Schindler\'s List', desc: 'In Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.', elmHeight: 0 },
-  { rank: 7, title: '12 Angry Men', desc: 'A dissenting juror in a murder trial slowly manages to convince the others that the case is not as obviously clear as it seemed in court.', elmHeight: 0 },
-  { rank: 8, title: 'The Good, the Bad and the Ugly', desc: 'A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.', elmHeight: 0 },
-  { rank: 9, title: 'The Lord of the Rings: The Return of the King', desc: 'Gandalf and Aragorn lead the World of Men against Sauron\'s army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.', elmHeight: 0 },
-  { rank: 10, title: 'Fight Club', desc: 'An insomniac office worker looking for a way to change his life crosses paths with a devil-may-care soap maker and they form an underground fight club that evolves into something much, much more...', elmHeight: 0 },
-]
-var data = [
-  originalData[0],
-  originalData[1],
-  originalData[2],
-  originalData[3],
-  originalData[4],
-  originalData[5],
-  originalData[6],
-  originalData[7],
-  originalData[8],
-  originalData[9],
-]
+class IndexList {
+  // 数据管理
+  data = {
+    nextKey: 11,
+    margin: 8,
+    sortBy: 'rank',
+    totalHeight: 0
+  }
 
-function changeSort(prop) {
-  sortBy = prop
-  data.sort((a, b) => {
-    if (a[prop] > b[prop]) {
-      return 1
-    }
-    if (a[prop] < b[prop]) {
-      return -1
-    }
-    return 0
-  })
-  render()
-}
+  // 排序
+  changeSort(prop) {
+    this.data.sortBy = prop
+    data.sort((a, b) => {
+      if (a[prop] > b[prop]) {
+        return 1
+      }
+      if (a[prop] < b[prop]) {
+        return -1
+      }
+      return 0
+    })
+    this.render()
+  }
 
-function add() {
-  var n = originalData[Math.floor(Math.random() * 10)]
-  data = [{ rank: nextKey++, title: n.title, desc: n.desc, elmHeight: 0 }].concat(data)
-  render()
-  render()
-}
+  // 添加
+  add() {
+    const n = data[Math.floor(Math.random() * 10)]
+    data = [{ rank: this.data.nextKey++, title: n.title, desc: n.desc, elmHeight: n.elmHeight }].concat(data)
+    this.render()
+  }
 
-function remove(movie) {
-  data = data.filter((m) => {
-    return m !== movie
-  })
-  render()
-}
+  // 删除
+  remove(movie) {
+    data = data.filter(m => m !== movie)
+    this.render()
+  }
 
-function movieView(movie) {
-  return h('div.row', {
-    key: movie.rank,
-    style: {
-      opacity: '0',
-      transform: 'translate(-200px)',
-      delayed: { transform: `translateY(${movie.offset}px)`, opacity: '1' },
-      remove: { opacity: '0', transform: `translateY(${movie.offset}px) translateX(200px)` }
-    },
-    hook: { insert: (vnode) => { movie.elmHeight = vnode.elm.offsetHeight } },
-  }, [
-    h('div', { style: { fontWeight: 'bold' } }, movie.rank),
-    h('div', movie.title),
-    h('div', movie.desc),
-    h('div.btn.rm-btn', { on: { click: () => remove(movie) } }, 'x'),
-  ])
-}
+  // 移除后更新视图
+  movieView(that, item) {
+    return h('li.list-group-item', {
+      key: item.rank,
+      style: {
+        opacity: '0',
+        transform: 'translate(-200px)',
+        delayed: { transform: `translateY(${item.offset}px)`, opacity: '1' },
+        remove: { opacity: '0', transform: `translateY(${item.offset}px) translateX(200px)` }
+      },
+      hook: { insert: (vnode) => { item.elmHeight = vnode.elm.offsetHeight } },
+    }, [
+      h('div', { style: { fontWeight: 'bold' } }, item.rank),
+      h('div', item.title),
+      h('div', item.desc),
+      h('div.btn.rm-btn', { on: { click: () => that.remove(item) } }, 'x'),
+    ])
+  }
 
-function render() {
-  data = data.reduce((acc, m) => {
-    var last = acc[acc.length - 1]
-    m.offset = last ? last.offset + last.elmHeight + margin : margin
-    return acc.concat(m)
-  }, [])
-  totalHeight = data.length === 0
-    ? 0
-    : data[data.length - 1].offset + data[data.length - 1].elmHeight
-  vnode = patch(vnode, view(data))
-}
+  // 渲染函数
+  render() {
+    const that = this
+    data = data.reduce((acc, m) => {
+      var last = acc[acc.length - 1]
+      m.offset = last ? last.offset + last.elmHeight + that.data.margin : that.data.margin
+      return acc.concat(m)
+    }, [])
+    that.data.totalHeight = data.length === 0 ? 0 : data[data.length - 1].offset + data[data.length - 1].elmHeight
+    vnode = patch(vnode, that.view(data))
+  }
 
-function view(data) {
-  return h('div', [
-    h('h1', 'Top 10 movies'),
-    h('div', [
-      h('a.btn.add', { on: { click: add } }, 'Add'),
-      'Sort by: ',
-      h('span.btn-group', [
-        h('a.btn.rank', { class: { active: sortBy === 'rank' }, on: { click: () => changeSort('rank') } }, 'Rank'),
-        h('a.btn.title', { class: { active: sortBy === 'title' }, on: { click: () => changeSort('title') } }, 'Title'),
-        h('a.btn.desc', { class: { active: sortBy === 'desc' }, on: { click: () => changeSort('desc') } }, 'Description'),
+  // 视图
+  view(data) {
+    const that = this;
+    return h('main.container', [
+      h('section.header-wrap', [
+        h('h1', 'Top 10 movies'),
+        h('div.clearfix', [
+          h('label', '排序方式：'),
+          h('div.btn-group', [
+            h('button.btn.btn-light', {
+              class: { active: that.data.sortBy === 'rank' },
+              on: { click: () => that.changeSort('rank') }
+            }, '等级'),
+            h('button.btn.btn-light', {
+              class: { active: that.data.sortBy === 'title' },
+              on: { click: () => that.changeSort('title') }
+            }, '名称'),
+            h('button.btn.btn-light', {
+              class: { active: that.data.sortBy === 'desc' },
+              on: { click: () => that.changeSort('desc') }
+            }, '描述')
+          ]),
+          h('button.btn.btn-secondary.float-right.btn-success', { on: { click: () => that.add() } }, '添加'),
+        ])
       ]),
-    ]),
-    h('div.list', { style: { height: totalHeight + 'px' } }, data.map(movieView)),
-  ])
+      h('section.list-wrap', [
+        h('ul.list-group', { style: { height: that.data.totalHeight + 'px' } },
+          data.map(item => that.movieView(that, item)))
+      ])
+    ])
+  }
 }
+
+const list = new IndexList();
 
 window.addEventListener('DOMContentLoaded', () => {
-  var container = document.getElementById('container')
-  vnode = patch(container, view(data))
-  render()
+  const container = document.getElementById('container')
+  vnode = patch(container, list.view(data))
+  list.render()
 })
