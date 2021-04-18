5a3197b601e50da8eb587172d71b58fe214f617b
张伟 <myzony@qq.com>
2021-04-18 22:21:14 +0800

    commit

--- part3/task-4/code/gridsome-pro/gridsome.config.js
+++ part3/task-4/code/gridsome-pro/gridsome.config.js
@@ -7,27 +7,10 @@
 module.exports = {
   siteName: 'Gridsome',
   plugins: [{
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'BlogPost',
-        path: './content/blog/**/*.md'
-      }
-    },
-    {
-      use: '@gridsome/source-strapi',
-      options: {
-        apiURL: 'http://localhost:1337',
-        queryLimit: 1000, // Defaults to 100
-        contentTypes: ['post'],
-        // singleTypes: ['impressum'],
-        // Possibility to login with a Strapi user,
-        // when content types are not publicly available (optional).
-        // loginData: {
-        //   identifier: '',
-        //   password: ''
-        // }
-      }
-
+    use: '@gridsome/source-filesystem',
+    options: {
+      typeName: 'BlogPost',
+      path: './content/blog/**/*.md'
     }
-  ]
+  }]
 }
